2b48050659f18abf0381a0d611e35c1292d3531c
pranav s <dev.pranabsk@gmail.com>
2022-08-22 12:11:50 +0530

    first commit

---
+++ src/components/User.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type UserProps = {name: string; age: number, isDone: boolean};
+
+//const User = (props: UserProps) => {
+const User = ({name, age, isDone}: UserProps) => {//destructure
+    return (
+        <div>
+            <h3>{name}</h3>
+            <h3>{age} years old</h3>
+            {isDone? <p>done</p> : <p>not done</p>}
+        </div>
+    );
+};
+
+export default User;
